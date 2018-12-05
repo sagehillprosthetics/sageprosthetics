c0edb4719184d3495f54286264fa35346d244d0b
Tim <timg51237@gmail.com>
2018-12-04 22:50:00 -0800

    Rudimentary Animations

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import * as types from '../redux/types';
+import posed from 'react-pose';
 //import { Parallax, Background } from 'react-parallax';
 import { Parallax, ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
 import Accordion from 'grommet/components/Accordion';
 import AccordionPanel from 'grommet/components/AccordionPanel';
 import Button from 'grommet/components/Button';
+import anime from 'animejs';
+import Transition from 'react-transition-group/Transition';
+import Particles from 'react-particles-js';
+import { Router } from '../routes';
 
 import '../styles.scss';
 
 class LandingPage extends Component {
+    state = {
+        isVisible: false
+    };
+
     static async getInitialProps({ req, store }) {
         store.dispatch({
             type: types.CHANGE_PAGE,
@@ -48,12 +57,64 @@ class LandingPage extends Component {
         });
     }
 
+    componentDidMount() {
+        console.log('componentdidmount');
+        this.setState({ isVisible: true });
+    }
+
+    renderParticles = () => {
+        return (
+            <Particles
+                style={{ height: '40vh' }}
+                params={{
+                    particles: {
+                        number: {
+                            value: 70
+                        },
+                        size: {
+                            value: 3
+                        },
+                        color: {
+                            value: '#2cc99d'
+                        },
+                        move: {
+                            enable: true,
+                            direction: 'right',
+                            out_mode: 'out',
+                            bounce: false,
+                            speed: 3,
+                            attract: {
+                                enable: false,
+                                rotateX: 0,
+                                rotateY: 0
+                            }
+                        },
+                        line_linked: {
+                            distance: 140,
+                            color: '#2cc99d'
+                        }
+                    },
+                    interactivity: {
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: 'repulse'
+                            }
+                        }
+                    }
+                }}
+            >
+                HELLO
+            </Particles>
+        );
+    };
+
     renderAccordian = () => {
         return (
             <Accordion
                 style={{
                     fontWeight: '400',
-                    marginTop: '40px',
+                    margin: '0 20% 0 20%',
                     borderWidth: '0px',
                     width: '60%'
                 }}
@@ -103,7 +164,11 @@ class LandingPage extends Component {
     };
 
     render() {
-        const copy = 'Parallax'.split('');
+        const Animation = posed.div({
+            visible: { opacity: 1 },
+            hidden: { opacity: 0 }
+        });
+
         return (
             <ParallaxProvider>
                 <title> Sage Prosthetics </title>
@@ -124,48 +189,57 @@ class LandingPage extends Component {
                         },
                         {
                             children: (
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        width: '100vw'
-                                    }}
-                                >
-                                    <img
-                                        src="/static/printer.png"
-                                        style={{ margin: '100px 0 100px 0' }}
-                                        alt="gradient"
-                                    />
-                                </div>
+                                <Transition onEnter={animatePrinterIn} timeout={1000} in={this.state.isVisible}>
+                                    <div
+                                        style={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            width: '100vw',
+                                            opacity: 0
+                                        }}
+                                        className="printer"
+                                    >
+                                        <img
+                                            src="/static/1-21-squashed.png"
+                                            style={{ margin: '150px 0 100px 0', width: '50%', height: '50%' }}
+                                            alt="gradient"
+                                        />
+                                    </div>
+                                </Transition>
                             ),
                             amount: 0.1,
                             slowerScrollRate: true
                         },
                         {
                             children: (
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        width: '100vw',
-                                        fontWeight: '700',
-                                        textAlign: 'center',
-                                        fontSize: '550%',
-                                        margin: '15% 0 0 10px'
-                                    }}
-                                >
+                                <Transition onEnter={animateHeadingIn} timeout={0} in={this.state.isVisible}>
                                     <div
                                         style={{
-                                            color: '#7ed4c6',
-                                            lineHeight: '100%',
-                                            background: '-webkit-linear-gradient(-45deg, #7f5ac5, #558ad2, #75c4a0)',
-                                            WebkitBackgroundClip: 'text',
-                                            WebkitTextFillColor: 'transparent'
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            width: '100vw',
+                                            fontWeight: '900',
+                                            textAlign: 'center',
+                                            fontSize: '367%',
+                                            margin: '14% 0 0 10px',
+                                            opacity: 0
                                         }}
+                                        className="heading"
                                     >
-                                        WE MAKE <br /> HANDS
+                                        <div
+                                            style={{
+                                                color: '#7ed4c6',
+                                                lineHeight: '100%',
+                                                background:
+                                                    '-webkit-linear-gradient(left, #9357cc 0%,#2989d8 50%,#2cc99d 100%)',
+                                                WebkitBackgroundClip: 'text',
+                                                WebkitTextFillColor: 'transparent'
+                                            }}
+                                        >
+                                            WE MAKE <br /> HANDS
+                                        </div>
                                     </div>
-                                </div>
+                                </Transition>
                             ),
                             amount: 0.1,
                             slowerScrollRate: false
@@ -197,33 +271,75 @@ class LandingPage extends Component {
                                         display: 'flex',
                                         alignItems: 'center',
                                         flexDirection: 'column',
-                                        width: '100vw',
-                                        fontWeight: '700',
-                                        textAlign: 'center',
-                                        margin: '15% 0 0 10px'
+                                        justifyContent: 'center',
+                                        width: '100vw'
                                     }}
                                 >
-                                    <div
-                                        style={{
-                                            lineHeight: '70%',
-                                            background: '-webkit-linear-gradient(-45deg, #7f5ac5, #558ad2, #75c4a0)',
-                                            WebkitBackgroundClip: 'text',
-                                            WebkitTextFillColor: 'transparent',
-                                            fontSize: '300%'
-                                        }}
-                                    >
-                                        WE USE 3D PRINTING TO{' '}
+                                    <div style={{ width: '100vw', padding: '0px', margin: '0px' }}>
+                                        <div
+                                            style={{
+                                                position: 'absolute',
+                                                width: '100vw',
+                                                height: '40vh'
+                                            }}
+                                        >
+                                            {this.renderParticles()}
+                                        </div>
+                                        <div
+                                            style={{
+                                                fontWeight: '700',
+                                                textAlign: 'center',
+                                                margin: '15% 0 10% 10px',
+                                                zIndex: 30
+                                            }}
+                                        >
+                                            <Transition
+                                                onEnter={animateSubheadingIn}
+                                                timeout={0}
+                                                in={this.state.isVisible}
+                                            >
+                                                <div
+                                                    style={{
+                                                        lineHeight: '70%',
+                                                        background:
+                                                            '-webkit-linear-gradient(left, #9357cc 30%,#2989d8 50%,#2cc99d 70%)',
+                                                        WebkitBackgroundClip: 'text',
+                                                        WebkitTextFillColor: 'transparent',
+                                                        fontSize: '300%'
+                                                    }}
+                                                    className="subheading"
+                                                >
+                                                    WE USE 3D PRINTING TO{' '}
+                                                </div>
+                                            </Transition>
+                                            <Transition
+                                                onEnter={animateMainheadingIn}
+                                                timeout={0}
+                                                in={this.state.isVisible}
+                                            >
+                                                <div
+                                                    style={{
+                                                        fontSize: '500%',
+                                                        lineHeight: '100%'
+                                                    }}
+                                                    className="mainheading"
+                                                >
+                                                    {' '}
+                                                    MAKE PROSTHETICS{' '}
+                                                </div>
+                                            </Transition>
+                                        </div>
                                     </div>
                                     <div
                                         style={{
-                                            fontSize: '500%',
-                                            lineHeight: '100%'
+                                            backgroundColor: 'white',
+                                            zIndex: '30',
+                                            width: '100%',
+                                            justifyContent: 'center'
                                         }}
                                     >
-                                        {' '}
-                                        MAKE PROSTHETICS{' '}
+                                        {this.renderAccordian()}
                                     </div>
-                                    {this.renderAccordian()}
                                 </div>
                             ),
                             amount: 0.1,
@@ -303,15 +419,18 @@ class LandingPage extends Component {
                                     >
                                         Connecting people who make hands <br /> with people who need them.
                                     </div>
-                                    <Button
-                                        label="Learn more about e-NABLE"
+                                    <Box
+                                        style={{ margin: '40px', padding: '15px', borderWeight: '2px' }}
                                         href="http://enablingthefuture.org/"
-                                        critical={true}
-                                        style={{
-                                            color: 'white',
-                                            margin: '20px'
-                                        }}
-                                    />
+                                        onClick={() => (window.location.href = 'http://enablingthefuture.org/')}
+                                    >
+                                        Learn more about e-NABLE
+                                        {/* <Button
+                                            label="Learn more about e-NABLE"
+                                            critical={true}
+                                            style={{ color: '#ffffff' }}
+                                        /> */}
+                                    </Box>
                                 </div>
                             ),
                             amount: 0.1,
@@ -326,6 +445,85 @@ class LandingPage extends Component {
         );
     }
 }
+
+const Box = posed.button({
+    hoverable: true,
+    init: {
+        borderColor: '#FFFFFF',
+        color: '#ffffff',
+        backgroundColor: 'rgb(0,0,0,0)'
+    },
+    hover: {
+        //borderColor: '#000000',
+        color: '#000000',
+        backgroundColor: '#ffffff'
+    }
+});
+
+const animateMainheadingIn = mainheading => {
+    return anime({
+        targets: mainheading,
+        opacity: {
+            value: [0, 1]
+        },
+        filter: ['blur(7px)', 'blur(0px)'],
+        translateY: [500, 0],
+        rotate: {
+            value: [90, 0]
+        },
+        easing: 'easeOutQuint',
+        duration: 1000
+    });
+};
+
+const animateSubheadingIn = subheading => {
+    return anime({
+        targets: subheading,
+        opacity: {
+            value: [0, 1]
+        },
+        filter: ['blur(7px)', 'blur(0px)'],
+        scale: {
+            value: [10, 1]
+        },
+        easing: 'easeOutQuint',
+        duration: 800
+    });
+};
+
+const animatePrinterIn = printer => {
+    console.log('something happened');
+    return anime({
+        targets: printer,
+        opacity: {
+            value: [0, 1]
+        },
+        filter: ['blur(7px)', 'blur(0px)'],
+        scale: {
+            value: 1.3
+        },
+        easing: 'easeOutQuint',
+        duration: 1800
+    });
+};
+
+const animateHeadingIn = heading => {
+    console.log('something happened');
+    return anime({
+        targets: heading,
+        opacity: {
+            value: [0, 1]
+        },
+        filter: ['blur(7px)', 'blur(0px)'],
+        scale: {
+            value: 1.4,
+            duration: 1800
+        },
+        translateY: -15,
+        easing: 'easeOutQuart',
+        duration: 1900
+    });
+};
 
 const styles = {
     dropdown: {
